8001a3bd97144379c909500af5a54b2e7dfd450a
Philipp A. <flying-sheep@web.de>
2025-12-29 11:57:03 +0100

    feat: add `random` method (#19)

--- src/pandas_uuid/__init__.py
+++ src/pandas_uuid/__init__.py
@@ -25,6 +25,7 @@ if TYPE_CHECKING:
     from typing import Self
 
     import numpy.typing as npt
+    from numpy.random import Generator
     from numpy.typing import NDArray
 
     npt._ArrayLikeInt_co = None  # type: ignore  # noqa: PGH003, SLF001
@@ -112,6 +113,12 @@ class UuidDtype(ExtensionDtype):
     storage: UuidStorage = field(default_factory=_default_storage_kind)
     """Storage kind, either `"numpy"` or `"pyarrow"`."""
 
+    def __post_init__(self) -> None:
+        """Validate storage kind."""
+        if self.storage not in {"numpy", "pyarrow"}:
+            msg = f"storage must be 'numpy' or 'pyarrow', not {self.storage}"
+            raise ValueError(msg)
+
     # ExtensionDtype essential API (3 class attrs and methods)
 
     @cached_property
@@ -178,7 +185,14 @@ class BaseUuidArray(ExtensionArray, abc.ABC):
         del boxed  # part of the API
         # pandas converts to numpy object array before calling this,
         # so we need to convert it back.
-        return lambda b: str(_to_uuid_numpy(b))
+        return lambda b: str(b if pd.isna(b) else _to_uuid_numpy(b))
+
+    # Custom API
+
+    @classmethod
+    @abc.abstractmethod
+    def random(cls, size: int, *, rng: int | np.random.Generator | None = None) -> Self:
+        """Generate an array of random UUIDs."""
 
 
 class UuidArray(BaseUuidArray, NumpyExtensionArray):
@@ -338,6 +352,15 @@ class UuidArray(BaseUuidArray, NumpyExtensionArray):
 
         return pa.array(self._ndarray, type=type)  # pyright: ignore[reportReturnType]
 
+    # Custom API
+
+    @override
+    @classmethod
+    def random(cls, size: int, *, rng: int | Generator | None = None) -> Self:
+        rng = np.random.default_rng(rng)
+        values = rng.bytes(size * 16)
+        return cls._simple_new(np.frombuffer(values, dtype=_UUID_NP_STORAGE_DTYPE))
+
 
 class ArrowUuidArray(BaseUuidArray, ArrowExtensionArray):
     """Extension array for storing uuid data in a :class:`pyarrow.ChunkedArray`."""
@@ -374,7 +397,7 @@ class ArrowUuidArray(BaseUuidArray, ArrowExtensionArray):
 
         if isinstance(values, pa.Array | pa.ChunkedArray):
             self._pa_array = (
-                pa.chunked_array([values.cast(pa.uuid())])
+                pa.chunked_array([values.view(pa.uuid())])
                 if isinstance(values, pa.Array)
                 else values.cast(pa.uuid())
             )  # pyright: ignore[reportAttributeAccessIssue]
@@ -432,9 +455,8 @@ class ArrowUuidArray(BaseUuidArray, ArrowExtensionArray):
                 and isinstance(item.start, int | None)
                 and isinstance(item.stop, int | None)
             ):
-                start = item.start if item.start is not None else 0
-                length = item.stop - start if item.stop is not None else None
-                values = self._pa_array.slice(start, length)
+                r = range(len(self))[item]
+                values = self._pa_array.slice(r.start, r.stop - r.start)
             case slice():
                 item = np.array(range(len(self._pa_array))[item])
                 values = self._pa_array.take(item)
@@ -483,3 +505,17 @@ class ArrowUuidArray(BaseUuidArray, ArrowExtensionArray):
             other._pa_array.cast(pa.binary(16)),
         )
         return cast("BooleanArray", pd.array(result, dtype="boolean"))  # pyright: ignore[reportArgumentType]
+
+    # Custom API
+
+    @override
+    @classmethod
+    def random(cls, size: int, *, rng: int | Generator | None = None) -> Self:
+        import pyarrow as pa
+
+        # pyarrow’s random generator only does non-NaN floats, we want unbiased
+        rng = np.random.default_rng(rng)
+        values = rng.bytes(size * 16)
+        buf_vals = pa.py_buffer(values)
+        arr = pa.Array.from_buffers(pa.uuid(), size, [None, buf_vals])
+        return cls(cast("pa.UuidArray", arr))
